bfd36cd504bc67746d9c5e2e8e179e8eb4abe82c
Fredrik Löwenhamn <lowet84@gmail.com>
2018-08-28 10:03:45 +0200

    Updated demo app

--- app/index.js
+++ app/index.js
@@ -1,17 +1,22 @@
 // content of index.js
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello world!')
+  if(request.url==='/data'){
+    response.end('changed')
+  }
+
+  var file =  fs.readFileSync('index.html', 'utf8');
+  response.end(file)
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    console.log('something bad happened', err)
   }
 
   console.log(`server is listening on ${port}`)
