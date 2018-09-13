c287bb4fb989ee9bb7516ea560a6ec8de6927e14
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-09-13 12:51:49 +0000

    Love coding? here's the secret reason why

--- cheat.sh
+++ cheat.sh
@@ -3,6 +3,5 @@
 sh reset.sh
 docker network create core
 docker run --rm -it -v dockercidemo_oauth2proxy:/data alpine sh -c "echo "fredrik.lowenhamn@gmail.com" > /data/emails"
-cp template.env .env
-sed -i -re "s/[(]your\ domain[)]/fredriklowenhamn.com/g" .env
+echo SERVER_DOMAIN=fredriklowenhamn.com > .env
 docker-compose up -d
