6b130200c97a618e8f736c849364661e98c08328
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-09-13 12:46:38 +0000

    Spinning up the hamster...

--- cheat.sh
+++ cheat.sh
@@ -4,5 +4,5 @@ sh reset.sh
 docker network create core
 docker run --rm -it -v dockercidemo_oauth2proxy:/data alpine sh -c "echo "fredrik.lowenhamn@gmail.com" > /data/emails"
 cp template.env .env
-sed -i -e 's/\(your\ domain\)/fredriklowenhamn.com/g' .env
+sed -i -re "s/[(]your\ domain[)]/fredriklowenhamn.com/g" .env
 docker-compose up -d
--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker image ls -q)
+#docker rmi -f $(docker image ls -q)
 docker volume rm dockercidemo_drone dockercidemo_gitlab_config dockercidemo_gitlab_data dockercidemo_gitlab_logs dockercidemo_gogs dockercidemo_gogs_ssh dockercidemo_oauth2proxy dockercidemo_portainer
 docker network rm core
 rm .env
