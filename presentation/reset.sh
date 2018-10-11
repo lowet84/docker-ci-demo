1a337ef9c20fb58c0747054651b7eab189f0c9ec
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-10-11 06:51:43 +0000

    A long time ago, in a galaxy far far away...

--- presentation/reset.sh
+++ presentation/reset.sh
@@ -5,4 +5,4 @@ docker rm -f $(docker ps -a -q)
 docker volume rm dockercidemo_drone dockercidemo_gitlab_config dockercidemo_gitlab_data dockercidemo_gitlab_logs dockercidemo_gogs dockercidemo_gogs_ssh dockercidemo_portainer
 docker network rm core
 rm .env
-sh splash.sh
+sh presentation/splash.sh
